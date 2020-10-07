b6516aa5442a15958b72a5ccb9441842efc6b701
jessevictor <kongoijesse97@gmail.com>
2020-10-07 17:26:01 +0300

    updating our views

--- news/models.py
+++ news/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True, null =True, default = "")
+
+    def __str__(self):
+        return self.first_name
+    def save_editor(self):
+        self.save()
+    class Meta:
+        ordering = ['first_name']
+
+
+ #tag mode
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+
+    #Class Articles
+class Article(models.Model):
+    title = models.CharField(max_length =60, default='')
+    post = models.TextField()
+    editor = models.ForeignKey('Editor', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def todays_news(cls):
+        today = dt.date.today()
+        news = cls.objects.filter(pub_date_date = today)
+        return news
+
+    @classmethod
+    def days_news(cls,date):
+        news = cls.objects.filter(pup_date__date = date)
+        return news
+
+
+    def __str__(self):
+        return self.title
+
+
+
+    # Testing Save Method
+    # def test_save_method(self):
+    #     self.james.save_editor()
+    #     editors = Editor.objects.all()
+    #     self.assertTrue(len(editors) > 0)
--- tribune/settings.py
+++ tribune/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'tribune.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tribune',
+        'USER': 'moringa',
+        'PASSWORD': 'jesse1234',
     }
 }
 
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
